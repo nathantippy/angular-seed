a3fb578ad91ad13b28a55e5d56c2fc4853a158a0
nathantippy <nathan@tippy.name>
2014-04-28 23:30:27 -0500

    page layout work, found better print solution.

--- app/js/app.js
+++ app/js/app.js
@@ -57,6 +57,15 @@ app.controller('MyCtrl', (function($scope,stickers) {
           
       };
       
-      
+      $scope.printDiv = function(divName) {
+          var printContents = document.getElementById(divName).innerHTML;
+          var originalContents = document.body.innerHTML;
+
+          document.body.innerHTML = printContents;
+
+          window.print();
+
+          document.body.innerHTML = originalContents;
+     }
       
   }));
